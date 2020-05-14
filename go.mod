06118d5ce24fc75ba505cba9558c4f8564b4a15e
Kalle M. Aagaard <git@k-moeller.dk>
2020-05-14 14:00:48 +0200

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/KalleDK/go-certmgr
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/pkg/sftp v1.11.0
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+)
---
+++ main.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/BurntSushi/toml"
+	"github.com/docopt/docopt-go"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	DefaultPort       = 22
+	DefaultConfigPath = "/etc/certmgr/certmgr.conf"
+	DefaultPrivateKey = "/etc/certmgr/id_rsa"
+	DefaultStore      = "/var/certmgr"
+)
+
+type Config struct {
+	Store      string
+	Hostname   string
+	User       string
+	Port       int
+	PrivateKey string
+
+	ServerCert string
+	ServerKey  string
+	Fullchain  string
+
+	ReloadCommand   string
+	ReloadArguments []string
+}
+
+func (c Config) lastmodified() string {
+	return path.Join(c.Store, "lastmodified")
+}
+
+type Client struct {
+	*sftp.Client
+}
+
+func (c *Client) ReadFile(p string) ([]byte, error) {
+	remoteFile, err := c.Open(p)
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(remoteFile)
+}
+
+func (c *Client) DownloadFile(source, dest string) error {
+	data, err := c.ReadFile(source)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(dest, data, 0644)
+}
+
+func newSSHClient(conf *Config) (*ssh.Client, error) {
+
+	pemkey, err := ioutil.ReadFile(conf.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	signer, err := ssh.ParsePrivateKey([]byte(pemkey))
+	if err != nil {
+		return nil, fmt.Errorf("signer key %w", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: conf.User,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", conf.Hostname, conf.Port), config)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %w", err)
+	}
+
+	return client, nil
+}
+
+func newClient(conf *Config) (*Client, error) {
+	sshClient, err := newSSHClient(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := sftp.NewClient(sshClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{client}, nil
+}
+
+func loadConfig(configPath string) (*Config, error) {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var conf Config
+	if err := toml.Unmarshal(data, &conf); err != nil {
+		return nil, err
+	}
+
+	if conf.User == "" {
+		return nil, errors.New("missing: User")
+	}
+
+	if conf.Hostname == "" {
+		return nil, errors.New("missing: Hostname")
+	}
+
+	if conf.Port == 0 {
+		conf.Port = DefaultPort
+	}
+
+	if conf.PrivateKey == "" {
+		conf.PrivateKey = DefaultPrivateKey
+	}
+
+	if conf.Store == "" {
+		conf.Store = DefaultStore
+	}
+
+	if conf.Fullchain == "" {
+		conf.Fullchain = path.Join(conf.Store, "fullchain.crt")
+	}
+
+	if conf.ServerCert == "" {
+		conf.ServerCert = path.Join(conf.Store, "server.crt")
+	}
+
+	if conf.ServerKey == "" {
+		conf.ServerKey = path.Join(conf.Store, "server.key")
+	}
+
+	return &conf, nil
+}
+
+func compareLastmodified(client *Client, conf *Config) (bool, error) {
+	localDate, err := ioutil.ReadFile(conf.lastmodified())
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return false, err
+	}
+
+	remoteDate, err := client.ReadFile("lastmodified")
+	if err != nil {
+		return false, err
+	}
+
+	return bytes.Compare(remoteDate, localDate) != 0, nil
+}
+
+func renew(client *Client, conf *Config) error {
+	fmt.Println("renewing")
+
+	if err := client.DownloadFile("lastmodified", conf.lastmodified()); err != nil {
+		return err
+	}
+
+	if err := client.DownloadFile("server.pem", conf.ServerCert); err != nil {
+		return err
+	}
+
+	if err := client.DownloadFile("server.key", conf.ServerKey); err != nil {
+		return err
+	}
+
+	if err := client.DownloadFile("fullchain.pem", conf.Fullchain); err != nil {
+		return err
+	}
+
+	if conf.ReloadCommand != "" {
+		fmt.Println("reloading")
+		out, err := exec.Command(conf.ReloadCommand, conf.ReloadArguments...).Output()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("=======\n%s\n======", out)
+	}
+
+	return nil
+}
+
+func main() {
+
+	usage := `Certmgr.
+
+Usage:
+  certmgr [--force] [--config=<config>]
+  certmgr -h | --help
+  certmgr --version
+
+Options:
+  -h --help                       Show this screen.
+  --version                       Show version.
+  -f, --force                     Force
+  -c <config>, --config=<config>  Path to config [default: /etc/certmgr/certmgr.conf].
+`
+	arguments, _ := docopt.ParseDoc(usage)
+	confPath, err := arguments.String("--config")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	force, err := arguments.Bool("--force")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf, err := loadConfig(confPath)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client, err := newClient(conf)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	shouldRenew, err := compareLastmodified(client, conf)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if !shouldRenew && !force {
+		fmt.Print("nothing to do")
+		return
+	}
+
+	err = renew(client, conf)
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
