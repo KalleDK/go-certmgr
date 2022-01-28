2aa48f3d35d6f163dd162af97e0ad17c44479e76
Kalle M. Aagaard <git@k-moeller.dk>
2022-01-28 02:53:19 +0100

    pfx

--- acmeserver/acmeserver.go
+++ acmeserver/acmeserver.go
@@ -1,6 +1,10 @@
 package acmeserver
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io/fs"
@@ -12,6 +16,7 @@ import (
 	"github.com/KalleDK/go-certapi/certapi/certserver"
 	"github.com/google/uuid"
 	"gopkg.in/ini.v1"
+	"software.sslmate.com/src/go-pkcs12"
 )
 
 type AuthDB interface {
@@ -83,8 +88,11 @@ func (b *AcmeBackend) realname(domain string, ctype certapi.CertType) (string, e
 	switch ctype {
 	case certapi.Cert:
 		return fmt.Sprintf("%s.cer", domain), nil
+	case certapi.CertChain:
+		return "fullchain.cer", nil
 	case certapi.Key:
 		return fmt.Sprintf("%s.key", domain), nil
+
 	}
 	return "", errors.New("file not found")
 }
@@ -108,12 +116,7 @@ func (b *AcmeBackend) GetCertInfo(domain string, key certapi.APIKey) (certinfo c
 	return fromIni(data)
 }
 
-func (b *AcmeBackend) GetCertFile(domain string, t certapi.CertType, key certapi.APIKey) (cert certserver.CertFile, err error) {
-	if !b.auth.HasAccess(domain, t, key) {
-		err = errors.New("invalid API key")
-		return
-	}
-
+func (b *AcmeBackend) getPEM(domain string, t certapi.CertType) (cert certserver.CertFile, err error) {
 	sub, err := fs.Sub(b.fs, domain)
 	if err != nil {
 		return
@@ -137,6 +140,177 @@ func (b *AcmeBackend) GetCertFile(domain string, t certapi.CertType, key certapi
 	}
 
 	return cert, nil
+}
+
+func (b *AcmeBackend) getPFX(domain string) (cert certserver.CertFile, err error) {
+	sub, err := fs.Sub(b.fs, domain)
+	if err != nil {
+		return
+	}
+
+	key, err := func() (key crypto.PrivateKey, err error) {
+		name, err := b.realname(domain, certapi.Key)
+		if err != nil {
+			return
+		}
+
+		data, err := fs.ReadFile(sub, name)
+		if err != nil {
+			return
+		}
+
+		block, _ := pem.Decode(data)
+		if block == nil || block.Type != "RSA PRIVATE KEY" {
+			return nil, errors.New("failed to decode PEM block containing public key")
+		}
+
+		return x509.ParsePKCS1PrivateKey(block.Bytes)
+	}()
+	if err != nil {
+		return
+	}
+
+	pub, err := func() (pub *x509.Certificate, err error) {
+		name, err := b.realname(domain, certapi.Cert)
+		if err != nil {
+			return
+		}
+
+		stat, err := fs.Stat(sub, name)
+		if err != nil {
+			return
+		}
+		cert.ModTime = stat.ModTime()
+		cert.Size = stat.Size()
+
+		data, err := fs.ReadFile(sub, name)
+		if err != nil {
+			return
+		}
+
+		block, _ := pem.Decode(data)
+		if block == nil || block.Type != "CERTIFICATE" {
+			err = errors.New("failed to decode PEM block containing public key")
+			return
+		}
+
+		return x509.ParseCertificate(block.Bytes)
+	}()
+	if err != nil {
+		return
+	}
+
+	cert.Data, err = pkcs12.Encode(rand.Reader, key, pub, nil, pkcs12.DefaultPassword)
+	if err != nil {
+		return
+	}
+
+	return cert, nil
+
+}
+
+func (b *AcmeBackend) getPFXChain(domain string) (cert certserver.CertFile, err error) {
+	sub, err := fs.Sub(b.fs, domain)
+	if err != nil {
+		return
+	}
+
+	key, err := func() (key crypto.PrivateKey, err error) {
+		name, err := b.realname(domain, certapi.Key)
+		if err != nil {
+			return
+		}
+
+		data, err := fs.ReadFile(sub, name)
+		if err != nil {
+			return
+		}
+
+		block, _ := pem.Decode(data)
+		if block == nil || block.Type != "RSA PRIVATE KEY" {
+			return nil, errors.New("failed to decode PEM block containing public key")
+		}
+
+		return x509.ParsePKCS1PrivateKey(block.Bytes)
+	}()
+	if err != nil {
+		return
+	}
+
+	pub, cas, err := func() (pub *x509.Certificate, cas []*x509.Certificate, err error) {
+		name, err := b.realname(domain, certapi.Cert)
+		if err != nil {
+			return
+		}
+
+		stat, err := fs.Stat(sub, name)
+		if err != nil {
+			return
+		}
+		cert.ModTime = stat.ModTime()
+		cert.Size = stat.Size()
+
+		data, err := fs.ReadFile(sub, name)
+		if err != nil {
+			return
+		}
+
+		block, rest := pem.Decode(data)
+		if block == nil || block.Type != "CERTIFICATE" {
+			err = errors.New("failed to decode PEM block containing public key")
+			return
+		}
+
+		pub, err = x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return
+		}
+
+		cas = []*x509.Certificate{}
+		for len(rest) > 0 {
+			block, rest = pem.Decode(data)
+			if block == nil || block.Type != "CERTIFICATE" {
+				err = errors.New("failed to decode PEM block containing public key")
+				return
+			}
+			var ca *x509.Certificate
+			ca, err = x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				return
+			}
+			cas = append(cas, ca)
+		}
+
+		return pub, cas, nil
+	}()
+	if err != nil {
+		return
+	}
+
+	cert.Data, err = pkcs12.Encode(rand.Reader, key, pub, cas, pkcs12.DefaultPassword)
+	if err != nil {
+		return
+	}
+
+	return cert, nil
+
+}
+
+func (b *AcmeBackend) GetCertFile(domain string, t certapi.CertType, key certapi.APIKey) (cert certserver.CertFile, err error) {
+	if !b.auth.HasAccess(domain, t, key) {
+		err = errors.New("invalid API key")
+		return
+	}
+
+	switch t {
+	case certapi.PKCS12:
+		return b.getPFX(domain)
+	case certapi.PKCS12Chain:
+		return b.getPFXChain(domain)
+	default:
+		return b.getPEM(domain, t)
+	}
+
 }
 
 func NewHandler(subfs fs.FS, auth AuthDB, uid uuid.UUID) http.Handler {
--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.1
 	gopkg.in/ini.v1 v1.66.3
+	software.sslmate.com/src/go-pkcs12 v0.0.0-20210415151418-c5206de65a78
 )
