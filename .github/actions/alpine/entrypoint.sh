2389ec93d65da6f1edb4e04d9d041c46b6c53371
Kalle M. Aagaard <git@k-moeller.dk>
2022-02-01 01:43:03 +0100

    test

--- .github/actions/alpine/entrypoint.sh
+++ .github/actions/alpine/entrypoint.sh
@@ -7,8 +7,8 @@ export PACKAGER_PUBKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
 printf "${INPUT_ABUILD_KEY}" > "${PACKAGER_PRIVKEY}"
 printf "${INPUT_ABUILD_KEY_PUB}" > "${PACKAGER_PUBKEY}"
 cp "${PACKAGER_PUBKEY}" /etc/apk/keys/
-echo "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:1}/"
-sed -i "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:1}/" APKBUILD
+echo "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:11}/"
+sed -i "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:11}/" APKBUILD
 abuild -F checksum
 abuild -F -r
 find $REPODEST -name "*.apk" | xargs apk verify
